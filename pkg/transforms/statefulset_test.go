d0baccd688a9e510d4cf639ef4ccc93ceda32b62
Abigail M. Johnson <abigail.m.johnson@ibm.com>
2020-06-01 12:08:36 -0500

    add sts unavailable test and mock data

--- pkg/transforms/statefulset_test.go
+++ pkg/transforms/statefulset_test.go
@@ -22,3 +22,13 @@ func TestTransformStatefulSet(t *testing.T) {
 	AssertEqual("current", node.Properties["current"], int64(1), t)
 	AssertEqual("desired", node.Properties["desired"], int64(1), t)
 }
+
+func TestTransformStatefulSetUnavailable(t *testing.T) {
+	var s v1.StatefulSet
+	UnmarshalFile("statefulset-unavailable.json", &s, t)
+	node := StatefulSetResource{&s}.BuildNode()
+
+	// Test only the fields that exist in stateful set - the common test will test the other bits
+	AssertEqual("current", node.Properties["current"], int64(0), t)
+	AssertEqual("desired", node.Properties["desired"], int64(1), t)
+}
